efc6b7ed239fddcfeaf5d15f9315743860ee6e6f
lucaspauloop <lucaspaulodeoliveira@outlook.com>
2019-11-14 17:53:25 -0300

    update

--- src/pages/layout/box/form.js
+++ src/pages/layout/box/form.js
@@ -2,7 +2,6 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux';
 import { addTask } from '../../../actions/taskActions';
-import uuidv1 from 'uuid';
 
 // Components
 import Inputs from './form/input';
@@ -13,6 +12,13 @@ const mapDispatchToProps = dispatch => {
         addTask: task => dispatch(addTask(task))
     }
 };
+
+// Redux state to component props
+// const mapStateToProps = state => {
+//     return {
+//       task: state.taskReducer.task
+//     };
+//   };
 
 class Form extends Component {
     constructor() {
@@ -27,25 +33,52 @@ class Form extends Component {
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        //this.handleDelete = this.handleDelete.bind(this);
     }
 
+    componentDidMount() {
+        // Gets the id that is passed in url /:id
+        const { id } = this.props.match.params;
+        const { task } = this.props;
+    
+        // If it's passed an Id
+        if (id) {
+          // If there's already a task in redux store and it's equal to the id passed
+          if (task && task._id === id) {
+            this.fillInputWithTask(task);
+          } else {
+            // Otherwise, get the task from db
+            this.props.getTaskById(id);
+          }
+        }
+      }
+
+      componentDidUpdate() {
+        const { id } = this.props.match.params;
+        const { task } = this.props;
+    
+        // If it's passed an Id
+        if (id) {
+          // If there's already a task in redux store and state still without it
+          if (task && task._id === id && this.state._id === '') {
+            this.fillInputWithTask(task);
+          }
+        }
+      }
     // Updates a task if there's an Id passed
     // Creates a task if wasn't passed and Id
     handleSubmit(event) {
         event.preventDefault();
-
-        
-        this.state._id = uuidv1();
-
-        this.props.createTask(this.state);
-        
-        this.setState({
-            _id: '',
-            title: '',
-            details: '',
-            remember_me_date: '',
-        });
-    }
+    
+        if (this.state._id) {
+          this.props.updateTask(this.state);
+        } else {
+          this.props.addTask(this.state);
+        }
+    
+        // redirect to home page
+        this.props.history.push('/');
+      }
 
     handleChange(event) {
         this.setState({
@@ -54,7 +87,12 @@ class Form extends Component {
     }
 
     render() {
+        let { id } = this.props.match.params;
+        if (id === undefined) {
+            id = null;
+          }
         return (
+            
             <Fragment>
                 <h1 className="title has-text-grey-dark">
                     Create Task
@@ -68,4 +106,5 @@ class Form extends Component {
     }
 }
 
-export default connect( mapDispatchToProps)(Form);
+export default 
+    connect(mapDispatchToProps)(Form);
